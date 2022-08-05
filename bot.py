7069cdbd9c201c38e242d873b88c7d873b3bc098
streamtek <110534277+streamtek@users.noreply.github.com>
2022-08-05 23:21:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -42,14 +42,9 @@ async def sender_bH(event):
     for i in TO:
         try:
             string = event.message
-            new_string = string.replace('/', '')
-            new_string1 = new_string.replace('|', '')
-            new_string2 = new_string1.replace('@', '')
-            new_string3 = new_string2.replace('Target Profit', 'TP')
-            new_string4 = new_string3.replace('Closed', 'Close')
             await BotzHubUser.send_message(
                 i,
-                new_string4
+                new_string
             )
         except Exception as e:
             print(e)
